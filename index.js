c22fab028bee0afb8510f94a60eb6e277d658aa0
shomail <shomail1988@gmail.com>
2019-08-08 22:24:37 +0200

    eslint prettier support

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import {getHTML, getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
+import {
+  getHTML,
+  getTwitterFollowers,
+  getInstagramFollowers,
+} from './lib/scraper';
 
 async function go() {
-    const [twitterHTML, instagramHTML] = await Promise.all([
-        getHTML('https://twitter.com/Shomailbintahir'),
-        getHTML('https://www.instagram.com/shomail.b.tahir/')
-    ])
-    const twitterFollowers = await getTwitterFollowers(twitterHTML);
-    const instagramFollowers = await getInstagramFollowers(instagramHTML);
-    console.log(`You have ${twitterFollowers} twitter followers, and ${instagramFollowers} instagram followers`);
+  const [twitterHTML, instagramHTML] = await Promise.all([
+    getHTML('https://twitter.com/Shomailbintahir'),
+    getHTML('https://www.instagram.com/shomail.b.tahir/'),
+  ]);
+  const twitterFollowers = await getTwitterFollowers(twitterHTML);
+  const instagramFollowers = await getInstagramFollowers(instagramHTML);
+  console.log(
+    `You have ${twitterFollowers} twitter followers, and ${instagramFollowers} instagram followers`
+  );
 }
 
 go();
