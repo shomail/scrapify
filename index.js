4f31e38ad9ef3ef4eab89d50cacbce3071da0e95
shomail <shomail1988@gmail.com>
2019-08-08 09:48:47 +0200

    get twitter followers count

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import {getHTML} from './lib/scraper';
+import {getHTML, getTwitterFollowers} from './lib/scraper';
 
 async function go() {
-    getHTML()
+    const html = await getHTML('https://twitter.com/Shomailbintahir')
+    const tFollowers = await getTwitterFollowers(html);
+    console.log(`You have ${tFollowers} twitter followers`);
 }
 
 go();
