fab7e868f2f0fce1b52f849a6e6b0c54d04350f4
shomail <shomail1988@gmail.com>
2019-08-08 22:20:30 +0200

    instagram followers scrape

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import {getHTML, getTwitterFollowers} from './lib/scraper';
+import {getHTML, getTwitterFollowers, getInstagramFollowers} from './lib/scraper';
 
 async function go() {
-    const html = await getHTML('https://twitter.com/Shomailbintahir')
-    const tFollowers = await getTwitterFollowers(html);
-    console.log(`You have ${tFollowers} twitter followers`);
+    const [twitterHTML, instagramHTML] = await Promise.all([
+        getHTML('https://twitter.com/Shomailbintahir'),
+        getHTML('https://www.instagram.com/shomail.b.tahir/')
+    ])
+    const twitterFollowers = await getTwitterFollowers(twitterHTML);
+    const instagramFollowers = await getInstagramFollowers(instagramHTML);
+    console.log(`You have ${twitterFollowers} twitter followers, and ${instagramFollowers} instagram followers`);
 }
 
 go();
