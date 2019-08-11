7a526fdc4c8ab701fd1af1fbaaa2cc83661bf4d4
shomail <shomail1988@gmail.com>
2019-08-11 22:22:09 +0200

    refactor

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import {
-  getHTML,
-  getTwitterFollowers,
-  getInstagramFollowers,
-} from './lib/scraper';
+import { getIstagramCount, getTwitterCount } from './lib/scraper';
 
 async function go() {
-  const [twitterHTML, instagramHTML] = await Promise.all([
-    getHTML('https://twitter.com/Shomailbintahir'),
-    getHTML('https://www.instagram.com/shomail.b.tahir/'),
+  const [iCount, tCount] = await Promise.all([
+    getIstagramCount(),
+    getTwitterCount(),
   ]);
-  const twitterFollowers = await getTwitterFollowers(twitterHTML);
-  const instagramFollowers = await getInstagramFollowers(instagramHTML);
-  console.log(
-    `You have ${twitterFollowers} twitter followers, and ${instagramFollowers} instagram followers`
-  );
+
+  console.log(iCount, tCount);
 }
 
 go();
