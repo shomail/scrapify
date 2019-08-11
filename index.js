1248f464e5c96ce491ed4cd17445dbcffd7deea2
shomail <shomail1988@gmail.com>
2019-08-11 22:31:42 +0200

    express server running

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import express from 'express';
 import { getIstagramCount, getTwitterCount } from './lib/scraper';
 
-async function go() {
+const app = express();
+
+app.get('/scrape', async (req, res, next) => {
+  console.log('scraping');
   const [iCount, tCount] = await Promise.all([
     getIstagramCount(),
     getTwitterCount(),
   ]);
+  res.json({ iCount, tCount });
+});
 
-  console.log(iCount, tCount);
-}
-
-go();
+app.listen(2019, () => console.log(`Scrapify API running on port: 2019`));
